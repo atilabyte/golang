a6b8034d5f1e463e27b85cf821a9c135bbebe8bc
root <atilalima569@gmail.com>
2026-07-25 19:19:25 -0500

    ok

---
+++ cron.go
@@ -0,0 +1,35 @@
+package main
+
+
+
+
+//download   of  script  cron.sh
+
+
+
+
+import (
+
+
+"os"
+"fmt"
+"net/http"
+
+)
+
+
+
+func  main() {
+
+
+  cli :=  http.Client{}
+   
+ 
+
+  cli.Get("")
+
+ 
+}
+
+
+

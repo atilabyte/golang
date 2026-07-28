6f5078a035aeccae18a880e986e8c2cf5f7963e0
root <atilalima569@gmail.com>
2026-07-28 15:39:39 -0500

    ok

--- make.sh
+++ make.sh
@@ -10,6 +10,7 @@ export CGO_ENABLED=0 #bin is static
 go build  atila.go  proc.go  magic.go  cron.go  tgram.go raw.go down.go  exec.go  depend.go   
 
 
+../upx/upx atila
 
 #../ssh/go/bin/go build brute.go  tgram.go #compile brute
 

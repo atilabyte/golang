b1249a84e64e34703d149a0e97173ff0ddf537e9
root <atilalima569@gmail.com>
2026-07-29 13:10:29 -0500

    ok

--- tgram.go
+++ tgram.go
@@ -24,7 +24,7 @@ func bot(msg string ) {
 
 
 
-      time.Sleep  ( 4   *  time.Second)  //evite    too many requests 
+      time.Sleep  ( 10     *  time.Second)  //evite    too many requests 
 
 
 

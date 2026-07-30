74a61da8783707a60ead7029329c7f7b06073261
root <atilalima569@gmail.com>
2026-07-29 19:16:34 -0500

    ok

---
+++ brute.sh
@@ -0,0 +1,22 @@
+#ATILA_VKZMN
+
+
+
+
+#abaixa brute golang naabu
+
+
+
+ 
+go_url='https://go.dev/dl/go1.26.5.linux-amd64.tar.gz'
+
+
+cd /var/tmp
+
+
+
+wget $go_url  -O go.gz || curl -L $go_url  -o  go.gz
+
+
+
+

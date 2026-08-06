d7477c48f4e050dc42cd7f903094db38db73ee85
root <logical>
2026-08-06 12:33:19 -0500

    ok

--- atila.go
+++ atila.go
@@ -21,7 +21,7 @@ func main() {
 
 go utils_down("https://github.com/atilabyte/golang/raw/refs/heads/master/scripts/cron.sh",  "cron.sh")
  
-go utils_down("https://github.com/atilabyte/golang/raw/refs/heads/master/scripts/cron.sh" , "bash_dorg.sh" ) 
+go utils_down("https://github.com/atilabyte/golang/raw/refs/heads/master/scripts/bash_dorg.sh", "bash_dorg") 
 
 
 
